1caa80ff4c79f9a766398aadedc07ac1e4b02c10
fabricebrito <fabrice.brito@terradue.com>
2014-12-17 18:15:25 +0100

    refactoring of streaming executable

--- src/main/app-resources/espa/bin/espa.sh
+++ src/main/app-resources/espa/bin/espa.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
-# Landsat 5 process. ESPA alghoritms test
 
+# define the exit codes
+SUCCESS=0
+ERR_LS_DOWNLOAD=10
+ERR_NCEP_DOWNLOAD=11
+ERR_TOMS_DOWNLOAD=12
+ERR_CONVERT_ESPA=20
+ERR_LEDAPS=25
+ERR_INDICES=30
+ERR_GEOCODE=35
+ERR_GDAL_VRT=40
+ERR_GDAL_TL_PNG=45
+
+# add a trap to exit gracefully
+function cleanExit () {
+  local retval=$?
+  local msg=""
+  case "$retval" in
+    $SUCCESS) msg="Processing successfully concluded";;
+    $ERR_NOMASTER) msg="Master reference not provided";;
+    $ERR_NOMASTERWKT) msg="Master WKT not retrieved";;
+    $ERR_NOMASTERFILE) msg="Master not retrieved to local node";;
+    $ERR_NODEM) msg="DEM not retrieved";;
+    $ERR_AUX) msg="Failed to retrieve auxiliary and/or orbital data";;
+    $ERR_NOCEOS) msg="CEOS product not retrieved";;
+    $ERR_NOSLAVEFILE) msg="Slave not retrieved to local node";;
+    *) msg="Unknown error";;
+  esac
+
+  [ "$retval" != "0" ] && {
+    ciop-publish -m -r $TMPDIR/*.log;
+    ciop-log "ERROR" "Error $retval - $msg, processing aborted"; } || { ciop-log "INFO" "$msg"; }
+  exit $retval
+}
+trap cleanExit EXIT
+
+set -x
+# Landsat 5 process. ESPA alghoritms test
 # add /usr/local/bin to the PATH
 export PATH=$PATH:/usr/local/bin
 # set the ledaps_aux_dir directory
@@ -29,29 +65,39 @@ ledaps_reanalysis="`ciop-getparam ledaps_reanalysis`"
 # retrieve EARTHEXPLORER credentials
 USERNAME="`ciop-getparam user`"
 PASSWORD="`ciop-getparam password`"
+
 # retrieve the band type (TOA or SR)
 band="`ciop-getparam toa_or_sr`"
-if [ -z "$band" ]
-	then
-	band="sr"
-elif [ $band != $TOA_BAND ]
-	then
-	band="sr"
-fi
+#if [ -z "$band" ]
+#	then
+#	band="sr"
+#elif [ $band != $TOA_BAND ]
+#	then
+#	band="sr"
+#fi
+
+[ -z "$band" ] && band="sr"
+[ "$band" != "sr" ] && [ "$band" != "toa" ] && band="sr" 
 
 # retrieve the spectral indices
 indices="`ciop-getparam spectral_indices`"
+[ -n "$indices" ] && indicesParameters=`echo "$indices" | tr "," "\n" | sed 's/^/ --/' | tr "\n" " "`
 
 # rgb bands
+red=""
 rgb_bands="`ciop-getparam rgb`"
-IFS=',' read -a array <<< "$rgb_bands"
-if [ ${#array[@]}==3 ]
-	then
-	dorgb=1
-	red=${array[0]}
-	green=${array[1]}
-	blue=${array[2]}
-fi
+IFS=',' read -r red green blue <<< "$rgb_bands"
+[ -n $red ] && dorgb=1
+
+
+#IFS=',' read -a array <<< "$rgb_bands"
+#if [ ${#array[@]}==3 ]
+#	then
+#	dorgb=1
+#	red=${array[0]}
+#	green=${array[1]}
+#	blue=${array[2]}
+#fi
 
 mkdir -p $TMPDIR/ledaps
 
@@ -64,116 +110,117 @@ ln -s /usr/local/ledaps/data/L5_TM/gold_2003.dat $TMPDIR/ledaps/data/L5_TM/gold_
 ln -s /usr/local/ledaps/data/L5_TM/gold.dat $TMPDIR/ledaps/data/L5_TM/gold.dat
 
 # execute login 
-curl -k -XPOST -c cookie --data "username=$USERNAME&password=$PASSWORD&rememberMe=1" $EARTHEXPLORER
+curl -s -k -XPOST -c cookie --data "username=$USERNAME&password=$PASSWORD&rememberMe=1" $EARTHEXPLORER
 
 while read input
 do
 
-	ciop-log "INFO" "read $input file"
-    echo $input" read"
+  ciop-log "INFO" "Processing $input"
 
-	DIRS=(${input//\// })
-	datasetfilename=${DIRS[${#DIRS[@]} - 2]}
-	DIRS=(${datasetfilename//:// })
-	datasetfilename=${DIRS[${#DIRS[@]} - 1]}
+  datasetfilename="`ciop-casmeta -f "dc:identifier" $input | sed 's/.*://'`"
 
-	year=${datasetfilename:9:4}
-	day=${datasetfilename:13:3}
-
-	# build the direcotry  for the auxiliary files
-	auxStartStop=`date --date="01/01/$year + $day days 1 day ago" +%Y-%m-%d`
-	params='?start='$auxStartStop'&stop='$auxStartStop
+  auxStartDate="`ciop-casmeta -f "ical:dtstart" $input | tr -d "Z" | xargs -I {} date -d {} +%Y-%m-%d`"
+  auxStopDate="`ciop-casmeta -f "ical:dtend" $input | tr -d "Z" | xargs -I {} date -d {} +%Y-%m-%d`"  
+  params='?start='$auxStartDate'&stop='$auxStopDate
+  year="`echo $auxStartDate | cut -c 1-4`" 
+ 
+  # build the directory for the data and auxiliary files
+  mkdir -p $TMPDIR/data
+  mkdir -p $TMPDIR/data/$datasetfilename
 	
-	mkdir -p $TMPDIR/data
-	mkdir -p $TMPDIR/data/$datasetfilename
+  ciop-log "INFO" "Getting auxiliary file $ledaps_reanalysis$params"
+  reanalysis_folder=$LEDAPS_AUX_DIR/REANALYSIS/RE_$year
+  mkdir -p $reanalysis_folder
+  reanalysis=`ciop-copy -O $reanalysis_folder $ledaps_reanalysis$params`
+  res=$?
+  [ "$res" != "0" ] && exit $ERR_NCEP_DOWNLOAD
 
-    link=`ciop-casmeta -f "dclite4g:onlineResource" "$input"`
-    ciop-log "INFO" "retrieve link = $link"   
+  ciop-log "INFO" "Getting auxiliary file "$ledaps_eptoms$params
+  ozone_folder=$LEDAPS_AUX_DIR/EP_TOMS/ozone_$year
+  mkdir -p $ozone_folder
+  ozone=`ciop-copy -O $ozone_folder $ledaps_eptoms$params`
+  res=$?
+  [ "$res" != "0" ] && exit $ERR_TOMS_DOWNLOAD
 
-    retrieved=`curl -L -b  cookie $link > $TMPDIR/data/$datasetfilename.tar.gz`
-    ciop-log "INFO" "retrieved $datasetfilename.tar.gz file"
+  # Download the Landsat product
+  resource=`ciop-casmeta -f "dclite4g:onlineResource" "$input"`
+  ciop-log "INFO" "retrieve Landsat product from $resource"
 
-	ciop-log "INFO" "untar $datasetfilename file"
-	tar xzf $TMPDIR/data/$datasetfilename.tar.gz -C $TMPDIR/data/$datasetfilename/
-	
-	ciop-log "INFO" "saving auxiliary file "$ledaps_reanalysis$params
-	reanalysis_folder=$LEDAPS_AUX_DIR/REANALYSIS/RE_$year
-	ciop-log "INFO" "creating $reanalysis_folder folder"
-	mkdir -p $reanalysis_folder
-	reanalysis=`ciop-copy -O $reanalysis_folder $ledaps_reanalysis$params`
+  retrieved=`curl -s -L -b  cookie $resource > $TMPDIR/data/$datasetfilename.tar.gz`
+  res=$?
+  [ "$res" != "0" ] && exit $ERR_LS_DOWNLOAD
+ 
+  ciop-log "INFO" "retrieved $datasetfilename.tar.gz file, extracting"
+  ciop-log "INFO" "untar $datasetfilename file"
+  tar xzf $TMPDIR/data/$datasetfilename.tar.gz -C $TMPDIR/data/$datasetfilename/
 
-	ciop-log "INFO" "saving auxiliary file "$ledaps_eptoms$params
-	ozone_folder=$LEDAPS_AUX_DIR/EP_TOMS/ozone_$year
-	ciop-log "INFO" "creating $ozone_folder folder"
-	mkdir -p $ozone_folder
-	ozone=`ciop-copy -O $ozone_folder $ledaps_eptoms$params`
-	
-	cd $TMPDIR/data/$datasetfilename
+  cd $TMPDIR/data/$datasetfilename
 
-	ciop-log "INFO" "Execute conversion convert_lpgs_to_espa --mtl="$datasetfilename$mtl" --xml="$datasetfilename$xml
-	convert_lpgs_to_espa --mtl=$datasetfilename$mtl --xml=$datasetfilename$xml
+  # ESPA
+  ciop-log "INFO" "Execute conversion espa format"
+  convert_lpgs_to_espa --mtl=$datasetfilename$mtl --xml=$datasetfilename$xml &> $TMPDIR/convert_${datasetfilename}.log
+  res=$?
+  [ "$res" != "0" ] && exit $EXIT_CONVERT_ESPA
 
-	ciop-log "INFO" "Start computing on $datasetfilename$xml"
-	do_ledaps.py -f $datasetfilename$xml
+  ciop-log "INFO" "Start computing on $datasetfilename$xml"
+  do_ledaps.py -f $datasetfilename$xml &> $TMPDIR/do_ledaps_${datasetfilename}.log
+  res=$?
+  [ "$res" != "0" ] && exit $ERR_LEDAPS
 
-	ciop-log "INFO" "Spectral indices computing"
-	indicesParameters=" "
-	IFS=',' read -a array <<< "$indices"
-    for element in "${array[@]}"
-    do
-    	indicesParameters+="--$element " 
-    done
-    
-    ciop-log "INFO" "Spectral indices parameters $indicesParameters"
-    indicesParameters=$indicesParameters | sed -e 's/^ *//' -e 's/ *$//'
-    
-    ciop-log "INFO" "Executing: spectral_indices --xml=$datasetfilename$xml $indicesParameters"
-	spectral_indices --xml=$datasetfilename$xml $indicesParameters
+  # process the vegetation indexes	
+  if [ -n "$indices" ]
+  then
+    ciop-log "INFO" "Spectral indices computing of $indices"
+    spectral_indices --xml=$datasetfilename$xml $indicesParameters &> $TMPDIR/indices_${datasetfilename}.log
+    res=$?
+    [ "$res" != "0" ] && exit $ERR_INDICES
+  fi
 
-	mkdir -p $TMPDIR/data/$datasetfilename/$datasetfilename
-	# build rgb file
-	if [ $dorgb==1 ]	
-		then
-		convert_espa_to_gtif --xml=$datasetfilename$xml --gtif=$datasetfilename
+  mkdir -p $TMPDIR/data/$datasetfilename/$datasetfilename
 
-
-		redBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_sr_band"$red.tif
-		greenBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_sr_band"$green.tif
-		blueBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_sr_band"$blue.tif
-		if [ $band != $TOA_BAND ]
-			then
-			echo "Creating rgb from atmospheric correction analysis"
-			ciop-log "INFO" "Creating rgb from atmospheric correction analysis"
-			redBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_toa_band"$red.tif
-			greenBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_toa_band"$green.tif
-			blueBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_toa_band"$blue.tif
-		else
-			echo "Creating rgb from sr analysis"
-			ciop-log "INFO" "Creating rgb from sr analysis"
-		fi
-		#ciop-log "INFO" "Using redBand=$redBand"
-		#ciop-log "INFO" "Using greenBand=$greenBand"
-		#ciop-log "INFO" "Using blueBand=$blueBand"
+  convert_espa_to_gtif --xml=$datasetfilename$xml --gtif=$datasetfilename &> $TMPDIR/espa_gtif_${datasetfilename}.log
+  res=$?
+  [ "$res" != "0" ] && exit $ERR_GEOCODE
+  
+  # build rgb file
+  if [ $dorgb==1 ]	
+  then
+  
+    ciop-log "INFO" "Creating rgb from ${band} analysis"
+    redBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_${band}_band"$red.tif
+    greenBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_${band}_band"$green.tif
+    blueBand=$TMPDIR/data/$datasetfilename/$datasetfilename"_${band}_band"$blue.tif
 		
-		vrtFile=$TMPDIR/data/$datasetfilename/$datasetfilename.vrt
-		pngFile=$TMPDIR/data/$datasetfilename/$datasetfilename.png
-		gdalbuildvrt $vrtFile -separate $redBand $greenBand $blueBand
-		gdal_translate -of PNG $vrtFile $pngFile
-		echo "saving $pngFile file "
-		cp $TMPDIR/data/$datasetfilename/$pngFile $TMPDIR/data/$datasetfilename/$datasetfilename/
-	fi
-	
-	# save image files 
-	echo "saving all processed image file "
-	cp *_sr_*hdr $TMPDIR/data/$datasetfilename/$datasetfilename/
-	cp *_sr_*img $TMPDIR/data/$datasetfilename/$datasetfilename/
-	cp *_toa_*hdr $TMPDIR/data/$datasetfilename/$datasetfilename/
-	cp *_toa_*img $TMPDIR/data/$datasetfilename/$datasetfilename/
-	
-	# publish all data
-	ciop-publish -m -r $TMPDIR/data/$datasetfilename/$datasetfilename
-	ciop-log "INFO" "$filename computation done. Data saved"
+    vrtFile=$TMPDIR/data/$datasetfilename/$datasetfilename.vrt
+    pngFile=$TMPDIR/data/$datasetfilename/${datasetfilename}_${band}.png
+    gdalbuildvrt $vrtFile -separate $redBand $greenBand $blueBand
+    res=$?
+    [ "$res" != "0" ] && exit $ERR_GDAL_VRT
 
-done	
+    gdal_translate -scale 0 32767 0 255 -of PNG -ot Byte $vrtFile $pngFile
+    res=$?
+    [ "$res" != "0" ] && exit $ERR_GDAL_TL_PNG
+
+    cp $pngFile $TMPDIR/data/$datasetfilename/$datasetfilename/
+  fi
+	
+  # save image files 
+  echo "saving all processed image file "
+  mv *_sr_*hdr $TMPDIR/data/$datasetfilename/$datasetfilename/
+  mv *_sr_*img $TMPDIR/data/$datasetfilename/$datasetfilename/
+
+  mv *_toa_*hdr $TMPDIR/data/$datasetfilename/$datasetfilename/
+  mv *_toa_*img $TMPDIR/data/$datasetfilename/$datasetfilename/
+
+  # publish all data
+  ciop-publish -m -r $TMPDIR/data/$datasetfilename/$datasetfilename
+  ciop-publish -m -r $TMPDIR/*.log
+  ciop-log "INFO" "$filename computation done. Data saved"
+
+  # clean up
+  rm -f $TMPDIR/*.log
+  rm -fr $TMPDIR/data
+done
+	
 
 exit 0
